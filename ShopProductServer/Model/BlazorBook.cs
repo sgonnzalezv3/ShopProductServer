ef3be520dd2cf59e3fe5dbf8bf1a488abb9cd304
sgonnzalezv3 <60562811+sgonnzalezv3@users.noreply.github.com>
2021-06-27 13:14:16 -0500

    Add project files.

---
+++ ShopProductServer/Model/BlazorBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopProductServer.Model
+{
+    public class BlazorBook
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool Exhausted { get; set; }
+        public List<TypeBook> TypeBook { get; set; }
+    }
+}
---
+++ ShopProductServer/Model/TypeBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopProductServer.Model
+{
+    public class TypeBook
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
